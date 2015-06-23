c5992d82a2fe30ef380785e519e8c195f6fc1898
Fully34 <cfullinw34@gmail.com>
2015-06-23 12:05:18 -0600

    finished jquery practice

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ $(document).ready(function() {
                         /* ~~~ PART I ~~~ */ 
 //===========================================================================//
 
+//============================== Dynamic text ==============================//
+        
     $('.add').click(function() {
 
         $('body').append('<p> Hello </p>');
-
     });
 
 
@@ -17,6 +18,8 @@ $(document).ready(function() {
         $('p').last().remove();
     });
 
+//============================== Dynamic Title ==============================//
+        
     $('.title').click(function() {
 
         $('body').append('<h1> WHAT UP, FOOL! </h1>');
@@ -27,6 +30,9 @@ $(document).ready(function() {
         $('h1').last().remove();
     });
 
+
+//============================== Dynamic List ==============================//
+        
     $('.add-ul').click(function() {
 
         $('body').append('<ul> <li> hey </li> <li> yo </li> <li> hi </li> </ul>');
@@ -41,7 +47,9 @@ $(document).ready(function() {
                         /* ~~~ PART II ~~~ */ 
 //===========================================================================//
 
-    $('p').mouseover(function() {
+//========================= Change that <p> to pink =========================//
+        
+    $('body').on('mouseover', 'p', function() {
 
         $(this).css({
 
@@ -50,13 +58,71 @@ $(document).ready(function() {
         });
     });
 
+
+//============================== Adding "!" ==============================//
+        
     $('h1').mouseover(function() {
 
         $(this).append('!');
     });
 
+
+//============================== Link Stuff ==============================//
+        
     $('a').click(function() {
 
-        $(this)
+        if ( !confirm("Are you sure you want to go there?!") ) {
+
+            $(this).remove();
+            return false;
+        };
+    });
+
+
+//===========================================================================//
+                        /* ~~~ PART III ~~~ */ 
+//===========================================================================//
+
+//============================ Click le Button ==============================//
+
+    $('.popup').click(function() {
+
+        $('body').append('<div class="pop-container"><div class="center-pop"></div></div>');
+
+//============================== Style Popup ==============================//
+        $('.pop-container').css({
+
+            'background-color': 'rgba(0,0,0,0.9)',
+            'height': '100%',
+            'width': '100%',
+            'position': 'fixed',
+            'top': '0',
+            'left': '0',
+        });
+
+        $('.center-pop').css({
+
+            'position' : 'absolute',
+            'top' : '50%',
+            'transform' : 'translatey(-50%)',
+            'margin-right' : '25%',
+            'margin-left' : '25%',
+            'height' : '300px',
+            'width' : '50%',
+            'border' : '1px solid black',
+            'background-color' : '#ed828d',
+        });
+
+//========================== Add close button ==============================//
+        
+        $('.center-pop').append('<a class = "close" href="#">close</a>');
+
+//============================== Close popup ==============================//
+        
+        $('.close').on('click', function () {
+
+            $('.center-pop').remove();
+            $('.pop-container').remove();
+        });
     });
 });
